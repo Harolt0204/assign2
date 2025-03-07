d65c54589b5e2e34f724b2a54b8a4485b3dd9ca9
Harolt0204 <fharolt@gmail.com>
2025-03-07 15:06:02 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,92 @@
+/*
+ * CS106L Assignment 2: Marriage Pact
+ * Created by Haven Whitney with modifications by Fabio Ibanez & Jacob Roberts-Baca.
+ *
+ * Welcome to Assignment 2 of CS106L! Please complete each STUDENT TODO
+ * in this file. You do not need to modify any other files.
+ *
+ */
+
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_set>
+#include <cstdlib> 
+#include <ctime>   
+
+std::string kYourName = "Harolt Farinango"; // Don't forget to change this!
+
+/**
+ * Takes in a file name and returns a set containing all of the applicant names as a set.
+ *
+ * @param filename  The name of the file to read.
+ *                  Each line of the file will be a single applicant's name.
+ * @returns         A set of all applicant names read from the file.
+ *
+ * @remark Feel free to change the return type of this function (and the function
+ * below it) to use a `std::unordered_set` instead. If you do so, make sure
+ * to also change the corresponding functions in `utils.h`.
+ */
+std::set<std::string> get_applicants(std::string filename) {
+  std::set<std::string>applicants;
+  std::ifstream file(filename);
+  std::string name;
+  
+  while (std::getline(file, name)) {
+      applicants.insert(name);
+  }
+  
+  return applicants;
+
+}
+
+
+/**
+ * Takes in a set of student names by reference and returns a queue of names
+ * that match the given student name.
+ *
+ * @param name      The returned queue of names should have the same initials as this name.
+ * @param students  The set of student names.
+ * @return          A queue containing pointers to each matching name.
+ */
+std::queue<const std::string*> find_matches(std::string name, std::set<std::string>& students) {
+  std::queue<const std::string*>matches;
+  char a= name[0];
+  char b= name[name.find(" ")+1];
+  for (const std::string& student:students){
+    if (student[0] == a && student[student.find(" ") + 1] == b) {
+      matches.push(&student);
+    }
+  }
+  return matches;
+}
+
+/**
+ * Takes in a queue of pointers to possible matches and determines the one true match!
+ *
+ * You can implement this function however you'd like, but try to do something a bit
+ * more complicated than a simple `pop()`.
+ *
+ * @param matches The queue of possible matches.
+ * @return        Your magical one true love.
+ *                Will return "NO MATCHES FOUND." if `matches` is empty.
+ */
+std::string get_match(std::queue<const std::string*>& matches) {
+  if (matches.empty()) {
+      return "NO MATCHES FOUND.";
+  }
+
+  int d = std::rand() % matches.size();
+  for (int i = 0; i < d; i++) {
+      matches.pop();
+  }
+
+  return *matches.front();
+}
+
+
+/* #### Please don't remove this line! #### */
+#include "autograder/utils.hpp"
+
